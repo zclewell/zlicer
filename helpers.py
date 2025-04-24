8d397eac5458dc7dbceb7e9099afd6e5ada9591b
Zach Clewell <zclewell@gmail.com>
2025-04-23 20:59:37 -0700

    Modify decompose function
    Add heuristic of distance from last removed facet for picking next
    adjacent facet.
    
    Add optional depth parameter for early stopping.
    
    Add removed_set parameter to provide faster lookup when checking if
    a facet is already in the path

--- helpers.py
+++ helpers.py
@@ -1,78 +1,104 @@
+import numpy as np
+
+
 def facet_to_tuple(facet):
-  return tuple(map(tuple, facet))
+    return tuple(map(tuple, facet))
+
 
 def point_to_tuple(point):
-  return tuple(point)
+    return tuple(point)
+
 
 def get_adj_map(facets):
-  # Given a list of facets, return a map that given a facet returns all adjacent facets
-  point_map = dict()
+    # Given a list of facets, return a map that given a facet returns all adjacent facets
+    point_map = dict()
 
-  for facet in facets:
-    for point in facet:
-      point = point_to_tuple(point)
+    for facet in facets:
+        for point in facet:
+            point = point_to_tuple(point)
 
-      if point in point_map:
-        point_map[point].add(facet_to_tuple(facet))
-      else:
-        point_map[point] = {facet_to_tuple(facet)}
+            if point in point_map:
+                point_map[point].add(facet_to_tuple(facet))
+            else:
+                point_map[point] = {facet_to_tuple(facet)}
 
-  adj_map = dict()
+    adj_map = dict()
 
-  for facet in facets:
-    v1, v2, v3 = facet
+    for facet in facets:
+        v1, v2, v3 = facet
 
-    v1_facets = point_map[point_to_tuple(v1)]
-    v2_facets = point_map[point_to_tuple(v2)]
-    v3_facets = point_map[point_to_tuple(v3)]
+        v1_facets = point_map[point_to_tuple(v1)]
+        v2_facets = point_map[point_to_tuple(v2)]
+        v3_facets = point_map[point_to_tuple(v3)]
 
-    # For a facet to be adjacent it must share two points
-    v1v2_adj = v1_facets.intersection(v2_facets)
-    v1v3_adj = v1_facets.intersection(v3_facets)
-    v2v3_adj = v2_facets.intersection(v3_facets)
+        # For a facet to be adjacent it must share two points
+        v1v2_adj = v1_facets.intersection(v2_facets)
+        v1v3_adj = v1_facets.intersection(v3_facets)
+        v2v3_adj = v2_facets.intersection(v3_facets)
 
-    adj_facets = v1v2_adj.union(v1v3_adj).union(v2v3_adj)
+        adj_facets = v1v2_adj.union(v1v3_adj).union(v2v3_adj)
 
-    # Remove self from adjacent list
-    if facet_to_tuple(facet) in adj_facets:
-      adj_facets.remove(facet_to_tuple(facet))
+        # Remove self from adjacent list
+        if facet_to_tuple(facet) in adj_facets:
+            adj_facets.remove(facet_to_tuple(facet))
 
-    adj_map[facet_to_tuple(facet)] = list(adj_facets)
+        adj_map[facet_to_tuple(facet)] = list(adj_facets)
 
-  return adj_map
+    return adj_map
 
-def _decompose(path, adj_map):
-  if len(path) == len(adj_map):
-    # Fully decomposed stl
-    return path
+# Recursive variant of decompose
+# path: working decomposition set
+# adj_map: adjaceny map of all facets
+# removed_set: set containing all points in path so far
+# depth: Length of path to end recursion at. If set to None, length of adj_map will be use instead
 
-  # Look at facets adjacent to last added to path
-  for adjacent_facet in adj_map[path[-1]]:
-    # This facet is already in the path, skip
-    if adjacent_facet in path:
-      continue
 
-    rec = _decompose(path + [adjacent_facet], adj_map)
+def _decompose(path, adj_map, removed_set, depth):
+    if depth and len(path) == depth:
+        return path
+    if len(path) == len(adj_map):
+        # Fully decomposed stl
+        return path
 
-    # On the golden path, return
-    if rec:
-      return rec
+    # Look at facets adjacent to the last removed, from closet to furthest.
+    mean_of_last = np.array(path[-1]).mean(axis=0)
+    adjacent_facets = [(np.linalg.norm(
+        mean_of_last - np.array(adj).mean(axis=0)), adj) for adj in adj_map[path[-1]]]
+    adjacent_facets = sorted(adjacent_facets, key=lambda x: x[0])
+    adjacent_facets = [adjacent[1] for adjacent in adjacent_facets]
 
-  # Dead end
-  return None
+    for adjacent_facet in adjacent_facets:
+        # This facet is already in the path, skip
+        if adjacent_facet in removed_set:
+            continue
+
+        removed_set.add(adjacent_facet)
+        rec = _decompose(path + [adjacent_facet], adj_map, removed_set, depth)
+
+        # On the golden path, return
+        if rec:
+            return rec
+        removed_set.remove(adjacent_facet)
+
+    # Dead end
+    return None
 
 # Remove facet by facet such that a facet removed is always adjacent with the prior facet removed.
-def decompose(facets):
-  adj_map = get_adj_map(facets)
+# facets: list of facets to decompose.
+# depth: Length of path to end recursion at. If set to None, length of adj_map will be use instead
 
-  for facet in facets:
-    facet = facet_to_tuple(facet)
 
-    path = [facet]
+def decompose(facets, depth=None):
+    adj_map = get_adj_map(facets)
 
-    path = _decompose(path, adj_map)
+    for facet in facets:
+        facet = facet_to_tuple(facet)
 
-    if path:
-      return path
+        path = [facet]
 
-  raise Exception('Unable to find decomposition')
+        path = _decompose(path, adj_map, set(path), depth)
+
+        if path:
+            return path
+
+    raise Exception('Unable to find decomposition')
